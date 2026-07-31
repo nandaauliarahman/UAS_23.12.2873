a807eb03fcaa4bb846f2cda3d20492831257b476
Nanda <nandaauliararhahman2272222@gmail.com>
2026-07-31 16:27:29 +0700

    fix: use HTTPS URL in Vercel demo runtime

--- api/index.php
+++ api/index.php
@@ -12,5 +12,6 @@ putenv('DB_CONNECTION=sqlite');
 putenv('DB_DATABASE='.$runtimeDatabase);
 putenv('SESSION_DRIVER=array');
 putenv('CACHE_STORE=array');
+putenv('APP_URL=https://uas-23-12-2873.vercel.app');
 
 require __DIR__.'/../public/index.php';
